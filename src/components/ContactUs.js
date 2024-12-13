072c49fc6bc9c0de9069cb36520b8027905f8f71
David Kim <davdkimusic@yahoo.com>
2024-12-13 08:37:51 -0500

    updated owners

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,12 +1,10 @@
 function ContactUs() {
   return (
     <div className="font-bold text-2xl grid grid-cols-[auto_1fr] gap-2 mb-4">
-      <h1>office:</h1>
-      <h1>804-520-6512</h1>
       <h1>mobile:</h1>
-      <h1>804-337-6125</h1>
+      <h1>(804) 495-6877</h1>
       <h1>email:</h1>
-      <h1>woodbrokers@aol.com</h1>
+      <h1>woodbrokerscompanyinc@gmail.com</h1>
     </div>
   );
 }
